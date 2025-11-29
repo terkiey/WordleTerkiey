d5f81faa8ef71d41e683a806d4a5ab2940373978
Terkiey <terkiey@gmail.com>
2025-11-29 22:24:52 +0000

    Implemented SolutionsBrowserControl as a UserControl and adjusted grid definitions for MainWindow to scale differently when window is resized.

---
+++ AppWPF/MainWindow/SolutionsBrowser/SolutionsBrowserControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace AppWPF;
+
+/// <summary>
+/// Interaction logic for SolutionsBrowserControl.xaml
+/// </summary>
+public partial class SolutionsBrowserControl : UserControl
+{
+    public SolutionsBrowserControl()
+    {
+        InitializeComponent();
+    }
+}
