8067a3e13545fa02b7ef5b122daecebcee9a8e56
Terkiey <terkiey@gmail.com>
2025-12-07 16:14:21 +0000

    Imrpoved the sidebar buttons a bit.

---
+++ AppWPF/UI/SolutionsBrowser/SolutionTypePanels/SolutionsBrowserExamplePanel.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace AppWPF;
+/// <summary>
+/// Interaction logic for SolutionTypePanel.xaml
+/// </summary>
+public partial class SolutionTypePanel : UserControl
+{
+    public SolutionTypePanel()
+    {
+        InitializeComponent();
+    }
+}
